fe66b0e2d76ec658f32246a7842a323a2e26a1dc
tjc <takunda.chidanika@untucapital.co.zw>
2025-07-07 12:04:44 +0200

    update

--- database/database.php
+++ database/database.php
@@ -7,16 +7,7 @@
 
 $db_conn = mysqli_connect($host, $username, $password, $dbname);
 
-//$host = 'db';
-//$db   = 'classified_ads';
-//$user = 'classified_user';
-//$pass = 'classified_pass';
-//$port = 9081; // Use 3306 inside the container, 9081 from your host
-//
-//$db_conn = new mysqli($host, $user, $pass, $db, $port);
-
 if (!$db_conn) {
     die("Connection Failed: " . mysqli_connect_error());
 }
 
-// mysqli_close($db_conn);
